583497d086bd0e39e05aa27db1570529d5ab7e47
Maita K <70974171+17mk1@users.noreply.github.com>
2022-02-14 13:32:25 -0700

    Create Tests.java

---
+++ Tests.java
@@ -0,0 +1,10 @@
+public class Tests {
+	public static void main(String[] args) {
+		Pizza pizza1 = new Pizza("small", "single", "none","single", "none");
+
+        System.out.println(pizza1);
+
+
+	}
+
+}
